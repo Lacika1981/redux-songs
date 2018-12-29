c12535a6e99709aa34db89fb0f5ca5e6538384ef
Laszlo Varga <vargalaszlo1981@googlemail.com>
2018-12-29 23:32:01 +0000

    Initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux';
+
+const songReducer = () => {
+    return [
+        { title: 'Defqon.1 Weekend Festival 2018 | POWER HOUR', duration: '67:30'},
+        { title: 'Defqon.1 2018 | Peacock in Concert', duration: '33:03' },
+        { title: 'Defqon.1 2018 | Tweekacore & Darren Styles', duration: '44:59' }
+    ]
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if( action.type === 'SONG_SELECTED' ) {
+        return action.payload;
+    }
+    return selectedSong;
+}
+
+export default combineReducers({
+    songs: songReducer,
+    selectedsong: selectedSongReducer,
+})
